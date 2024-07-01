abcb0980858d03746424654fe076a53d44a61aba
rosves <lytween.victoire@etam.fr>
2024-07-01 21:05:24 +0200

    01/07/24 -Lytween

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import "./loginPage.css";
+import "./loginPage.scss";
+import Logo from '../assets/images/bitchest_logo_mob.png';
 
 const LoginForm = ({ switchToSignUp }) => {
   return (
+    <div id='main_container'>
+
     <div id='container-login'>
-      <img src=''/>
+      <img className='logo' src={Logo}/>
       <h2 id='login_main_title'>Login</h2>
       <form>
         <div>
          
-          <input placeholder="Email" type="email" required />
+          <input placeholder="Username" type="name" required />
         </div>
         <div>
           
@@ -18,7 +21,17 @@ const LoginForm = ({ switchToSignUp }) => {
         <button className='button_login' type="submit">Login</button>
       </form>
       <p>Don't have an account yet? <button className="button_link" onClick={switchToSignUp}>Get one !</button></p>
+     </div>
+     <div className='blue_part _right'>
+      <img className='logo' src={Logo}/>
+      <h1>Welcome to BitChest !</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed turpis imperdiet odio egestas consectetur. Suspendisse felis enim, pretium in nisi sed, vulputate interdum neque. Ut pharetra libero odio. Integer blandit eleifend molestie. Curabitur commodo, nibh lobortis faucibus maximus, nibh arcu laoreet orci, euismod aliquam sem libero sit amet urna. Aliquam porta quam eget risus aliquet, in gravida lectus pharetra. </p>
+   
     </div>
+    </div>
+
+
+   
   );
 };
 
--- src/components/signUpPage.js
+++ src/components/signUpPage.js
@@ -1,32 +1,44 @@
-import React from 'react';
-import "./loginPage.css";
+import React from "react";
+import "./loginPage.scss";
+import Logo from "../assets/images/bitchest_logo_mob.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
 const SignUpForm = ({ switchToLogin }) => {
   return (
-    <div id='container-login'>
-      <h2 id='login_main_title'>Sign Up</h2>
+    <div id="container-login">
+      <div
+        className="header">
+        {" "}
+        <button className="button_link" onClick={switchToLogin}>
+          <FontAwesomeIcon className="arrow_left" icon={faChevronLeft} />
+        </button>
+        <img className="logo logo_sign_up" src={Logo} />
+      </div>
+      <h2 id="login_main_title">Sign Up</h2>
       <form>
         <div>
-          
+          <input placeholder="Username" type="name" required />
+        </div>
+        <div>
           <input placeholder="Email" type="email" required />
         </div>
         <div>
-          
           <input placeholder="Password" type="password" required />
         </div>
         <div>
-          
           <input placeholder="Confirm password" type="password" required />
-       
-          <label class="container">I confirm that I am over 18.
-  <input type="checkbox" />
-  <span class="checkmark"></span>
-</label>
+
+          <label class="checkbox_container">
+            <input type="checkbox" /> <p className="age_testament">I confirm that I am over 18.</p>
+            <span class="checkmark"></span>
+          </label>
         </div>
-        
-        <button className='button_login' type="submit">Sign Up</button>
+
+        <button className="button_login" type="submit">
+          Sign Up
+        </button>
       </form>
-      <p>Already have an account? <button className="button_link" onClick={switchToLogin}>Login</button></p>
     </div>
   );
 };
