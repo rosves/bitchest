ed54dde59cc37889c3a39ff881069e3ac0a95575
rosves <lytween.victoire@etam.fr>
2024-07-02 15:07:08 +0200

    02/07/24 -Lytween

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,37 +1,61 @@
-import React from 'react';
+import React from "react";
 import "./loginPage.scss";
-import Logo from '../assets/images/bitchest_logo_mob.png';
+import Logo from "../assets/images/bitchest_logo_mob.png";
 
 const LoginForm = ({ switchToSignUp }) => {
   return (
-    <div id='main_container'>
+    // container global
+    <div id="main_container">
+{/* container desk */}
+      <div className="container-desk">
+        {/* container du login */}
+        <div id="container-login">
+          <img className="logo" src={Logo} />
+          <h2 id="login_main_title">Login</h2>
 
-    <div id='container-login'>
-      <img className='logo' src={Logo}/>
-      <h2 id='login_main_title'>Login</h2>
-      <form>
-        <div>
-         
-          <input placeholder="Username" type="name" required />
+          <form>
+            <div>
+              <input placeholder="Username" type="name" required />
+            </div>
+
+            <div>
+              <input placeholder="Password" type="password" required />
+            </div>
+
+            <button className="button_login" type="submit">
+              Login
+            </button>
+          </form>
+
+          <p>
+            Don't have an account yet?{" "}
+            <button className="button_link" onClick={switchToSignUp}>
+              Get one !
+            </button>
+          </p>
         </div>
-        <div>
-          
-          <input placeholder="Password" type="password" required />
+        {/* container desk - fin */}
         </div>
-        <button className='button_login' type="submit">Login</button>
-      </form>
-      <p>Don't have an account yet? <button className="button_link" onClick={switchToSignUp}>Get one !</button></p>
-     </div>
-     <div className='blue_part _right'>
-      <img className='logo' src={Logo}/>
-      <h1>Welcome to BitChest !</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed turpis imperdiet odio egestas consectetur. Suspendisse felis enim, pretium in nisi sed, vulputate interdum neque. Ut pharetra libero odio. Integer blandit eleifend molestie. Curabitur commodo, nibh lobortis faucibus maximus, nibh arcu laoreet orci, euismod aliquam sem libero sit amet urna. Aliquam porta quam eget risus aliquet, in gravida lectus pharetra. </p>
-   
-    </div>
-    </div>
+        {/* containner du login - fin */}
+        {/* blue part */}
+        <div className="blue_part _right">
+          <img className="logo" src={Logo} />
+          <h1>Welcome to BitChest !</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed
+            turpis imperdiet odio egestas consectetur. Suspendisse felis enim,
+            pretium in nisi sed, vulputate interdum neque. Ut pharetra libero
+            odio. Integer blandit eleifend molestie. Curabitur commodo, nibh
+            lobortis faucibus maximus, nibh arcu laoreet orci, euismod aliquam
+            sem libero sit amet urna. Aliquam porta quam eget risus aliquet, in
+            gravida lectus pharetra. 
+          </p>
+        </div>
+        {/* blue part - fin */}
+      
 
-
-   
+    </div>
+    // container global - fin
   );
 };
 
